2d58fd662a3e948d2b515695ec2f4fb14ef09abf
rmungar1209 <rmungar1209@g.educaand.es>
2024-01-28 19:18:15 +0100

    todo comentado (faltan algunos cambios)

---
+++ src/Compras.kt
@@ -0,0 +1,32 @@
+class Domiciliacion(var compras: List<List<Any>>){
+
+    /**
+     * La funcion separador toma el objeto (lista) predefinida en el main() y la recorrerá para añadir a otra lista los
+     * domicilios de los clientes sin que estos se repitan.
+     *
+     * @return datos: List<String> con los domicilios de los clientes.
+     */
+    fun separador(): List<String> {
+        val clientes = this.compras
+        val datos = mutableListOf<String>()
+        for (cliente in clientes){
+            for (domicilio in cliente){
+                if (domicilio == cliente[3] && !datos.contains(domicilio)) {
+                    datos.add(domicilio.toString())
+                }
+            }
+        }
+        return datos
+    }
+
+    /**
+     * Esta funcion recoge la lista de domicilios y los imprime por separado
+     *
+     * @param datos : List<String> lista con las direcciones de los clientes
+     */
+    fun imprimirDirecciones(datos:List<String>){
+        for (direccion in datos){
+            println(direccion)
+        }
+    }
+}
---
+++ src/Main.kt
@@ -0,0 +1,13 @@
+fun main(){
+    val compras = Domiciliacion(
+        listOf<List<Any>>(
+            listOf("Nuria Costa", 5, 12780.78, "Calle Las Flores 355"),
+            listOf("Jorge Russo", 7, 699, "Mirasol 218"),
+            listOf("Nuria Costa", 7, 532.90, "Calle Las Flores 355"),
+            listOf("Julián Rodriguez", 12, 5715.99, "La Mancha 761"),
+            listOf("Jorge Russo", 15, 958, "Mirasol 218"),
+        )
+    )
+    val datos = compras.separador()
+    compras.imprimirDirecciones(datos)
+}
